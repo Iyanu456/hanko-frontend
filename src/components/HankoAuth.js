b34e5eb84c290cc6902a01765ed2048d91621749
Iyanu456 <oyerindei13@gmail.com>
2023-10-07 23:43:29 +0100

    added useMemo

--- src/components/HankoAuth.js
+++ src/components/HankoAuth.js
@@ -1,4 +1,4 @@
-import { useEffect, useCallback } from "react";
+import { useEffect, useCallback, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import { register, Hanko } from "@teamhanko/hanko-elements";
 
